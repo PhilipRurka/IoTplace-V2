a6d9064680b4cb40dbdb822f3743644b79ebf725
Philip Rurka <philiprurka@gmail.com>
2019-09-10 13:55:39 -0400

    Push

---
+++ New Project/src/components/App/AppStyles.js
@@ -0,0 +1,8 @@
+// import React from 'react';
+import styled from '@emotion/styled';
+
+export const ST = {
+  ToggleTheme: styled.button`
+    background-color: red;
+  `
+};
--- New Project/src/components/Footer/Footer.js
+++ New Project/src/components/Footer/Footer.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const HealerHero = styled.h1({
+  borderRadius: '50%',
+  transition: 'transform 400ms ease-in-out',
+  boxSizing: 'border-box',
+  display: 'flex',
+  ':hover': {
+    transform: 'scale(1.2)'
+  }
+})
 
 class Footer extends React.Component {
-  
+
   render() {
     return (
-      <h1 className="footer-wrapper">Footer</h1>
+      <div>
+        <h1 className="footer-wrapper">Footer</h1>
+        <HealerHero>Testing</HealerHero>
+      </div>
     );
   };
 };
