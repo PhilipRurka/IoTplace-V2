4e68a8a18e7bafdc8487d1961661d4d499fe740b
Philip Rurka <philiprurka@gmail.com>
2019-10-11 13:53:33 -0400

    Updates

--- precache-manifest.e125d9c2395fd03a613c149d08e91cb8.js
+++ precache-manifest.e125d9c2395fd03a613c149d08e91cb8.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "466aa082abd10bfa624c0b7643d7904e",
+    "revision": "3e2886d174d7a0cd302e9c334a43065b",
     "url": "/IoTplace-V2/index.html"
   },
   {
-    "revision": "65d261a312e36cba25c7",
+    "revision": "6d77b483dff146599b00",
     "url": "/IoTplace-V2/static/css/main.548eb2fa.chunk.css"
   },
   {
-    "revision": "cf84325ce585ec51d2b3",
-    "url": "/IoTplace-V2/static/js/2.065123b5.chunk.js"
+    "revision": "02c8730e25ec2ddfe8ae",
+    "url": "/IoTplace-V2/static/js/2.e242d262.chunk.js"
   },
   {
-    "revision": "65d261a312e36cba25c7",
-    "url": "/IoTplace-V2/static/js/main.bc78da00.chunk.js"
+    "revision": "6d77b483dff146599b00",
+    "url": "/IoTplace-V2/static/js/main.9f56d94c.chunk.js"
   },
   {
     "revision": "9aefed3d9e1d77a85391",
