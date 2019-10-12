c78427c968febf334521205eaf63f1cdd1c17597
Philip Rurka <philiprurka@gmail.com>
2019-10-12 16:19:37 -0400

    Push to save

--- validation/src/components/Toaster/Toast/Toast.js
+++ validation/src/components/Toaster/Toast/Toast.js
@@ -1,16 +1,105 @@
 import React from 'react';
 import styled from '@emotion/styled/macro';
+import ColorTheme from '../../../themes/colors';
+
+/** Colors of the current theme. */
+let colorTheme;
 
 class Toast extends React.Component {
+  state = {
+    currentStep: null
+  };
 
-  ToastStyled = styled.div({
-    '& + div': { marginTop: '20px' }
+  Wrapper = styled.div({
+    position: 'relative',
+    display: 'table',
+    marginLeft: 'auto',
+    '& + div': { marginTop: '12px' }
+  });
+
+  containerStyled = () => ({
+    backgroundColor: colorTheme.secondaryBackground,
+    color: colorTheme.primaryCopy,
+    marginRight: '20px',
+    padding: '10px 50px 10px 25px',
+    border: `2px solid ${colorTheme.primaryBorder}`,
+    borderRadius: '10px',
+    boxShadow: `0 0 13px ${colorTheme.primaryShadow}`,
+
+    'label': {
+      margin: '0',
+      whiteSpace: 'noWrap'
+    }
+  });
+
+  InvisibleContainer = styled.div(() => {
+    const { containerStyled, state: { currentStep } } = this;
+    let animation = null;
+
+    if(currentStep === 1) {
+      animation = { right: '0' };
+
+    } else if(currentStep === 3) {
+      animation = { height: '0' };
+    };
+
+    return {
+      ...containerStyled(),
+      position: 'relative',
+      display: 'inline-block',
+      overflow: 'hidden',
+      marginLeft: 'auto',
+      right: 'calc(-100% - 20px)',
+      height: '48px',
+      transition: 'right 0.5s ease, height 0.5s ease',
+      ...animation
+    };
+  });
+
+  VisibleContainer = styled.div(() => {
+    const { containerStyled } = this;
+
+    return {
+      display: 'none',
+      ...containerStyled(),
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)'
+    };
+  });
+  
+  Xicon = styled.div({
+    fontSize: '20px',
+    fontWeight: 'bold',
+    position: 'absolute',
+    top: '14px',
+    right: '20px',
+    lineHeight: '16px',
+    cursor: 'pointer'
   });
 
   componentDidMount() {
+
+    const step1 = 100;
+    const step2 = step1 + 3000;
+    const step3 = step1 + step2 + 500;
+
     setTimeout(() => {
-      this.dismount();
-    }, 3000);
+      this.setState({ currentStep: 1 });
+    }, step1);
+
+    // setTimeout(() => {
+    //   this.setState({ currentStep: 2 });
+    // }, step2 );
+    
+    // setTimeout(() => {
+    //   this.setState({ currentStep: 3 });
+    // }, step3);
+
+    // setTimeout(() => {
+    //   this.dismount();
+    // }, 5000);
   };
 
   dismount = () => {
@@ -18,11 +107,46 @@ class Toast extends React.Component {
     dismountToast(id);
   };
 
-  render() {
-    const { ToastStyled, props: { children } } = this;
+  Content = () => {
+    const {
+      Xicon,
+      dismount,
+      props: {
+        label,
+        id
+      }
+    } = this;
 
     return (
-      <ToastStyled>{children}</ToastStyled>
+      <div>
+        <label>{label}</label>
+        <Xicon
+          onClick={() => (dismount(id))}
+        >X</Xicon>
+      </div>
+    );
+  };
+
+  render() {
+    const {
+      Wrapper,
+      InvisibleContainer,
+      VisibleContainer,
+      Content,
+      props: { theme }
+    } = this;
+
+    colorTheme = ColorTheme[theme];
+
+    return (
+      <Wrapper>
+        <InvisibleContainer>
+          <Content />
+          <VisibleContainer>
+            <Content />
+          </VisibleContainer>
+        </InvisibleContainer>
+      </Wrapper>
     )
   }
 };
--- validation/src/components/Toaster/Toaster.js
+++ validation/src/components/Toaster/Toaster.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import styled from '@emotion/styled/macro';
-import ColorTheme from '../../themes/colors';
 
 import Toast from './Toast/Toast';
-/** Colors of the current theme. */
-let colorTheme;
 
 class Toaster extends React.Component {
 
@@ -12,31 +9,6 @@ class Toaster extends React.Component {
     position: 'fixed',
     top: '20px',
     right: '0'
-  });
-  
-  Container = styled.div(() => ({
-    position: 'relative',
-    backgroundColor: colorTheme.secondaryBackground,
-    color: colorTheme.primaryCopy,
-    marginRight: '20px',
-    padding: '10px 50px 10px 25px',
-    border: `2px solid ${colorTheme.primaryBorder}`,
-    borderRadius: '10px',
-    boxShadow: `0 0 13px ${colorTheme.primaryShadow}`,
-  
-    'label': {
-      margin: '0'
-    }
-  }));
-  
-  Xicon = styled.div({
-    fontSize: '20px',
-    fontWeight: 'bold',
-    position: 'absolute',
-    top: '14px',
-    right: '20px',
-    lineHeight: '16px',
-    cursor: 'pointer'
   });
   
   dismountToast = (id) => {
@@ -47,16 +19,12 @@ class Toaster extends React.Component {
   render() {
     const { 
       Wrapper,
-      Container,
-      Xicon,
       dismountToast,
       props: {
         theme,
         toastList
       }
      } = this;
-
-    colorTheme = ColorTheme[theme];
 
     return (
       <Wrapper>
@@ -66,14 +34,9 @@ class Toaster extends React.Component {
             key={id}
             id={id}
             dismountToast={dismountToast}
-            >
-            <Container>
-              <label>{toastList[id]}</label>
-              <Xicon
-              onClick={() => (dismountToast(id))}
-              >X</Xicon>
-            </Container>
-          </Toast>
+            label={toastList[id]}
+            theme={theme}
+          />
           )
         })}
       </Wrapper>
