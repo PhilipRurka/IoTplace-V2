4be68f8ba55e2e316e2fb461259319ed7ee00633
Philip Rurka <philiprurka@gmail.com>
2019-10-13 10:32:27 -0400

    Updates

--- precache-manifest.055b1d9b014d410cdc70c1541e5cb2f1.js
+++ precache-manifest.055b1d9b014d410cdc70c1541e5cb2f1.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9b23f44e13e7d6241335f3623db21918",
+    "revision": "148bd23b492098e6f0da6bf322c1521c",
     "url": "/IoTplace-V2/index.html"
   },
   {
-    "revision": "9b79ab97319e014bdda7",
+    "revision": "82750462824cb50ecafc",
     "url": "/IoTplace-V2/static/css/main.548eb2fa.chunk.css"
   },
   {
-    "revision": "02c8730e25ec2ddfe8ae",
-    "url": "/IoTplace-V2/static/js/2.e242d262.chunk.js"
+    "revision": "8984df9a6a3d959f8d74",
+    "url": "/IoTplace-V2/static/js/2.ae70f67a.chunk.js"
   },
   {
-    "revision": "9b79ab97319e014bdda7",
-    "url": "/IoTplace-V2/static/js/main.19389cdf.chunk.js"
+    "revision": "82750462824cb50ecafc",
+    "url": "/IoTplace-V2/static/js/main.24286097.chunk.js"
   },
   {
     "revision": "9aefed3d9e1d77a85391",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/IoTplace-V2/precache-manifest.89a03a183df0444b4f88adaab93c3f7e.js"
+  "/IoTplace-V2/precache-manifest.055b1d9b014d410cdc70c1541e5cb2f1.js"
 );
 
 self.addEventListener('message', (event) => {
